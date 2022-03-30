9823417106722060b75413a6607998a229032c11
liekeveldkamp <102236409+liekeveldkamp@users.noreply.github.com>
2022-03-30 10:21:20 +0000

    step 2 & 3

--- README.md
+++
@@ -1,1 +0,0 @@
-Hi, I'm Lieke Veldkamp, first-year Industrial Design Engineering at TU Delft
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 # Import the object Flask from the flask module
 from flask import Flask
+from covid import download_summary, download_confirmed_per_country
+
+# Import and setup logging
+import logging
+log_format = "[%(levelname)s] - %(asctime)s : %(message)s in %(module)s:%(lineno)d"
+logging.basicConfig(filename='covid.log', format=log_format, level=logging.INFO)
+
+import json
 
 # Create a webserver object called 'COVID Dashboard' and keep track of it in the variable called server
 server = Flask('COVID Dashboard')
@@ -9,14 +17,23 @@ server = Flask('COVID Dashboard')
 # Define the function 'index()' and connect it to the route /
 def index():
 # Return the string "A nice COVID dashboard."
-  return "A nice COVID dashboard"
+  return server.send_static_file('index.html')
 
 # Define an HTTP route /summary to serve the summary chart
 @server.route('/summary')
 # Define the function 'serve_summary()' and connect it to the route /summary
 def serve_summary():
-# Return the string "A bar chart summary of COVID cases per country."
-  return "A bar chart summary of COVID cases per country"
+  """
+  Download the summary from the COVID19 API, extract the 'Countries' values (skip 'Globals'). The chart description is returned with template and data.
+  """
+  # Load json template from summary.json
+  json_template = json.load(open("templates/summary.json"))
+  # Download summary from the COVID API
+  json_data = download_summary()
+  # Add the data to the template
+  json_template["data"]["values"] = json_data["Countries"]
+  # Send the chart description to the client
+  return json_template
 
 # Define an HTTP route /new to serve the new count worldwide chart
 @server.route('/new')
@@ -30,7 +47,7 @@ def serve_summary_new():
 # Define the function 'serve_netherlands_history()' and connect it to the route /netherlands
 def serve_netherlands_history():
 # Return the string "An area chart of COVID cases over time in the Netherlands."
-  return "An area chart of COVID cases over time in the Netherlands"
+  return download_confirmed_per_country("netherlands")
 
 # Start the webserver
 server.run('0.0.0.0')
